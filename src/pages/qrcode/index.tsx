d14a22c4270a09cabd671be19c6f2ee5a70f2412
liushuang <liushuangbill@outlook.com>
2022-04-03 21:21:22 +0800

    add: qrcode

---
+++ src/pages/qrcode/index.tsx
@@ -0,0 +1,71 @@
+import { Button, Card, Input, message } from 'antd'
+import { memo, useRef, useState } from 'react'
+import { QRCodeCanvas } from 'qrcode.react'
+import { saveAs } from 'file-saver'
+
+function QrCode () {
+  const [str, setStr] = useState('')
+  const [size, setSize] = useState(128)
+  const sizeEl = useRef<any>(null)
+
+  const handleChange = (e: any) => {
+    setStr(e.target.value)
+  }
+
+  const handleSizeChange = () => {
+    const n = Number(sizeEl.current?.input.value)
+    if (!isNaN(n) && (n >= 128 && n <= 512)) {
+      setSize(n)
+    }
+  }
+
+  const exportQrCode = () => {
+    if (!str) {
+      message.error('请输入内容')
+      return
+    }
+    const canvas: any = document.querySelector('.page-qrcode canvas')
+    if (canvas) {
+      canvas.toBlob((blob: any) => {
+        saveAs(blob, 'qrcode.png')
+      })
+    }
+  }
+
+  return (
+    <div className="page-qrcode">
+      <Card>
+        <div>
+          输入：
+          <Input value={str} onChange={handleChange} />
+        </div>
+        <div style={{ margin: '20px 0' }}>
+          二维码大小（128-512）：
+          <Input.Group compact>
+            <Input ref={sizeEl} defaultValue={size} style={{ width: '200px' }} />
+            <Button onClick={handleSizeChange}>save</Button>
+          </Input.Group>
+        </div>
+        <div>
+          <Button onClick={exportQrCode}>导出二维码</Button>
+        </div>
+      </Card>
+      <Card>
+        {
+          str
+            ? (
+              <QRCodeCanvas
+                value={str}
+                size={size}
+              />
+            )
+            : (
+              null
+            )
+        }
+      </Card>
+    </div>
+  )
+}
+
+export default memo(QrCode)
